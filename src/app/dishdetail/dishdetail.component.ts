eccdcba7b29c5494d5a1690ea52864d31dbee742
Abdul Rahman <rahmanimran8121999@gmail.com>
2022-12-07 09:32:08 +0530

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,11 +9,25 @@ import { Feedback, ContactType } from '../shared/feedback';
 import { Observable, of } from 'rxjs';
 import { Comment } from '../shared/comment';
 // import { visibility, flyInOut, expand } from ''
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden',style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -64,7 +78,7 @@ export class DishdetailComponent implements OnInit {
     .subscribe((dishIds) => this.dishIds = dishIds,
     errmess => this.errMess = <any>errmess );
     // this.dishService.getDishes().subscribe(dishes => this.dishes = dishes);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+    this.route.params.pipe(switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishservice.getDish(params['id']);}))
     .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
       errmess => this.errMess = <any>errmess );
       console.log(this.errMess);

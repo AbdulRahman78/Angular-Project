f1b772a2cf8d4b233fc2dd0c4ac01608df46e401
Abdul Rahman <rahmanimran8121999@gmail.com>
2022-12-09 11:36:31 +0530

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,10 +3,19 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Leader } from '../shared/Leader';
 import { LeaderService } from '../services/leader.service';
+import {  flyInOut, expand } from '../animations/app.animation';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display:block'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
